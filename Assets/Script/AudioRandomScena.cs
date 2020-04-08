cf3accb587e7bb5cd502ea59723ea00b21a4d535
kleberpucha <kpuchaortiz@gmail.com>
2020-04-07 23:50:06 -0500

    Agrgacion de Audio
    Audio Randomico para cada escena se podra agregar mas aundios o modificarlos

---
+++ Assets/Script/AudioRandomScena.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioRandomScena : MonoBehaviour
+{
+    public AudioClip[] clips;
+    public AudioMixerGroup output;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+            PlaySound();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void PlaySound() {
+        int random = Random.Range(0,clips.Length);
+        AudioSource sourse = gameObject.AddComponent<AudioSource>();
+        sourse.clip = clips[random];
+        sourse.outputAudioMixerGroup = output;
+        sourse.Play();
+        Destroy(sourse, clips[random].length);
+    }
+}
