4a95e67f42c0f64e9baf1d10f6f2b0721dbf2167
kleberpucha <kpuchaortiz@gmail.com>
2020-04-10 18:02:07 -0500

    Animacion de ambiente
    Se creo un scrip que pueeda ir cambiando es estado del dia segun la hora del sistema con los manejos de skybox

---
+++ Assets/Script/SkyboxAmbientacion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkyboxAmbientacion : MonoBehaviour
+{
+    public Material[] skybox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        string horastring = DateTime.Now.Hour.ToString();
+        int horaint = int.Parse(horastring);
+        RenderSettings.skybox = skybox[horaint];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DateTime fecha = DateTime.Now;
+        print(fecha);
+       
+    }
+}
